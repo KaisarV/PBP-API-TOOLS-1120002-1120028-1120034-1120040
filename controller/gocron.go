4059c1fda5f3a175a2e761b3aced6afb735b01c1
Richard Leonardo <richard.rikat19@gmail.com>
2022-04-01 17:44:23 +0700

    goredis

--- controller/gocron.go
+++ controller/gocron.go
@@ -1,12 +1,17 @@
 package controllers
 
 import (
-	gomail "GolangTools/gomail"
+	"fmt"
 
 	"github.com/claudiu/gocron"
 )
 
 func Gocron(epgi string, kuser string) {
 	gocron.Start()
-	gocron.Every(10).Seconds().Do(gomail.SendMorningMail, epgi, kuser)
+	// gocron.Every(10).Seconds().Do(gomail.SendMorningMail, epgi, kuser)
+	gocron.Every(10).Seconds().Do(Sapa, epgi, kuser)
+}
+
+func Sapa(epgi string, kuser string) {
+	fmt.Print(epgi + kuser)
 }
